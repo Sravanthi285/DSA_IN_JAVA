030060ecb217ab0656f41853262e07191f123d08
Sravanthi Karnapu <sravanthikarnapu@gmail.com>
2026-07-30 21:45:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (28.23%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int n = word.length();
+        int ans = 0;
+
+        if (n <= 8) {
+            ans = n * 1;
+        } else if (n <= 16) {
+            ans = 8 * 1 + (n - 8) * 2;
+        } else if (n <= 24) {
+            ans = 8 * 1 + 8 * 2 + (n - 16) * 3;
+        } else {
+            ans = 8 * 1 + 8 * 2 + 8 * 3 + (n - 24) * 4;
+        }
+
+        return ans;
+        
+    }
+}
